180464c8791f4202c7ff97d8bebe4b0115bb3a0f
june <june.qualwebs@gmail.com>
2020-05-28 22:20:54 +0530

    React js COursera Application

---
+++ src/components/DishdetailsComponent.js
@@ -0,0 +1,227 @@
+import React, { Component } from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem, Button, Row, Label,
+    Modal, ModalHeader, ModalBody,
+} from 'reactstrap';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
+import { Control, Form, Errors } from 'react-redux-form';
+import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
+
+/* Will be Used to Render Comment of selected dish */
+
+
+function RenderComments({comments, postComment, dishId}) {
+    if (comments != null)
+        return (
+            <ul type="none" className="list-unstyled">
+                <h4 className="mt-1">Comments</h4>
+                <Stagger in>
+                        {comments.map((comment) => {
+                            return (
+                                <Fade in>
+                                <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                                </Fade>
+                            );
+                        })}
+                        </Stagger>
+                <li>
+     
+                <CommentForm dishId={dishId} postComment={postComment} /></li>
+            </ul>
+        )
+    else
+        return (
+            <div></div>
+        );
+        
+}
+
+/* Will be Used to Render selected Dish */
+function RenderDish({ dish }) {
+    if (dish != null)
+        return (
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+            <Card>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+            </FadeTransform>
+            
+        );
+    else
+        return (
+            <div></div>
+        );
+}
+
+const Dishdetail = (props) => {
+         
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
+    return (
+        /* Will render selected dish and it's comments */
+
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                <RenderComments comments={props.comments}
+        postComment={props.postComment}
+        dishId={props.dish.id}
+      />
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        
+
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
+    }
+    render() {
+
+        
+        const required = (val) => val && val.length;
+        const maxLength = (len) => (val) => !(val) || (val.length <= len);
+        const minLength = (len) => (val) => val && (val.length >= len);
+        return (
+            <div className="container">
+
+                <Button outline onClick={this.toggleModal}> <span className="fa fa-edit fa-lg"></span>Submit Comment</Button>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Comment</ModalHeader>
+                    <ModalBody>
+
+                        
+                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
+                           <Row className="form-group">
+                                <Label htmlFor="rating" md={2}>Rating</Label>
+
+                                <Control.select model=".rating" name="rating"
+                                    className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="author" md={2}>Author</Label>
+
+                                <Control.text model=".author" id="author" name="author"
+                                    placeholder="Your Name"
+                                    className="form-control"
+                                    validators={{
+                                        required, minLength: minLength(3), maxLength: maxLength(15)
+                                    }}
+                                />
+                                <Errors
+                                    className="text-danger"
+                                    model=".author"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}
+                                />
+
+                            </Row>
+
+                            <Row className="form-group">
+                                <Label htmlFor="comment" md={2}>Comment</Label>
+
+                                <Control.textarea model=".comment" id="comment" name="comment"
+                                    rows="6"
+                                    className="form-control" />
+
+                            </Row>
+                            <Button type="submit" color="primary">
+                                Add Comment
+                           </Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+
+            </div>
+        )
+    }
+
+
+}
+
+
+
+export default Dishdetail;
